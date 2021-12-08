9c86b786d801f3bb72382bca8db41e780035e372
MahmoudAhmedAli <nahmoudahmedali@gmail.com>
2021-12-08 17:14:53 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import Contact from './Contact';
+import Blog from './Blog';
+import About from './About';
+import NAV from './NAV';
+function App() {
+  return (
+    <div className="App">
+     <NAV />
+     <Home />
+    </div>
+  );
+}
+
+export default App;
